661e9d3f071abe3ef46f5afdf79662d6dabb7591
Kian <108917081+kontobbbsbaan@users.noreply.github.com>
2022-11-06 07:19:51 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,34 +10,34 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 global.hituet = 0
-global.gopayno = "083144670363"
-global.danano = "085717497745"
-global.shopeepayno = "083144670363"
-global.creator = "6283144670363@s.whatsapp.net"
+global.gopayno = "6283845409710"
+global.danano = "6283845409710"
+global.shopeepayno = "6283845409710"
+global.creator = "6283845409710@s.whatsapp.net"
 global.thumb = fs.readFileSync(`./image/thumb.png`)
 global.qrisdonate = fs.readFileSync(`./image/qris.jpg`)
 global.fake = `📍𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑩𝒚 RENZ 𝑩𝒐𝒕 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑`
 global.sessionName = 'fangz'
 global.packname = ``
-global.author = `×͜× ▭▬▭▬▭▬ 👑 ▭▬▭▬▭▬ ×͜×\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗕𝘆 RENZ\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿 RENZ : 6283144670363\nﾒ 𝗬𝗼𝘂𝘁𝘂𝗯𝗲 : RENZ XD\n⊟————————⊟`
+global.author = `×͜× ▭▬▭▬▭▬ 👑 ▭▬▭▬▭▬ ×͜×\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗲𝗱 𝗕𝘆 MODZ BOTZ\nﾒ 𝗖𝗿𝗲𝗮𝘁𝗼𝗿 RENZ : 6283845409710\nﾒ 𝗬𝗼𝘂𝘁𝘂𝗯𝗲 : MODZ BOTZ OFC\n⊟————————⊟`
 global.antilink = false
 global.antiwame = false
 global.autodltt = false
 global.autosticker = false
-global.ownerNomor = '6283144670363'
-global.ownerName = 'Creator RENZ'
-global.ownerNumber = ["6283144670363@s.whatsapp.net"]
+global.ownerNomor = '6283845409710'
+global.ownerName = 'Creator bot'
+global.ownerNumber = ["6283845409710@s.whatsapp.net"]
 global.cek1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 global.mess = {
-    wait: 'Wait Sis Please be patient',
-    succes: 'Good Luck Sis ?',
+    wait: 'Loading...',
+    succes: 'Succes',
     admin: 'Group Admin Special Features!!!',
     botAdmin: 'Bots Must Be Admins First!!!',
     owner: 'Ente Kadang-kadang Ente, Nih Khusus Owner Nih!!!',
     group: 'Features Used Only For Groups!!!',
     private: 'Features Used Only For Private Chat!!!',
     bot: 'Bot Number User Special Features!!!',
-    error: 'Error Sis, Please Chat Owner...',
+    error: 'Error...',
     premium: 'Maaf Sebelumya Kamu Belum Premium, Silahkan Pencet Di Bawah Untuk Beli Premium',
     endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
 }
